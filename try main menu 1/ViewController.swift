78618ed7c13cdb868dbace83b96a8f16325a0f17
ufoxcode <ufoxcode@gmail.com>
2020-01-18 16:38:55 +0800

    Initial Commit

---
+++ try main menu 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try main menu 1
+//
+//  Created by UFO Xcode on 2020/1/18.
+//  Copyright © 2020 UFO Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
